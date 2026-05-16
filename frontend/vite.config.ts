2740cb91d20202d19ed5d880416bd59792c03122
Aschefr <s.asche@yahoo.fr>
2026-05-16 22:34:40 +0200

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: 'all',
+		allowedHosts: true,
 		fs: {
 			allow: ['..']
 		},
